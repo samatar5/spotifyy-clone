8aff6d867f518eee59974fbbc0d21017348ec137
Samatar5 <samatar5mohamed@gmail.com>
2023-03-27 15:01:30 +0200

    can now fetch playlist

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,27 @@
-import '@/styles/globals.css'
+import { redirectURL } from "@/config";
+import { getAccessTokenFromUrl } from "@/utils/GetAccessTokenFromUrl";
+import "@/styles/globals.css";
+import SpotifyWebApi from "spotify-web-api-node";
+import { useRouter } from "next/router";
+export const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+  redirectUri: redirectURL,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  if (typeof window !== "undefined" && router.asPath !== "/login") {
+    const accessToken =
+      getAccessTokenFromUrl() || sessionStorage.getItem("spotify-key");
+    console.log(accessToken);
+
+    if (accessToken) {
+      sessionStorage.setItem("spotify-key", accessToken);
+      spotifyApi.setAccessToken(accessToken);
+    } else {
+      router.replace("/login");
+    }
+    fe;
+  }
+  return <Component {...pageProps} />;
 }
