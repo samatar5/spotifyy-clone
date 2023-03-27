34959a41ed4ad0f635708187d6d728dbbb1ee2a2
Samatar5 <samatar5mohamed@gmail.com>
2023-03-27 11:13:24 +0200

    added color theme to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: "#000000",
+        "bg-dimmed": "#121212",
+        text: "#ffffff",
+        "text-dimmed": "#b3b3b3",
+        primary: "#2f6",
+      },
+    },
   },
   plugins: [],
 };
