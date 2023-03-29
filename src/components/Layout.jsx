0134648f64be61630886792315cbeb79be268a64
Samatar5 <samatar5mohamed@gmail.com>
2023-03-29 17:46:23 +0200

    fixed grid

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
     <div className="flex h-screen flex-col  ">
       <div className="flex flex-1 overflow-y-auto ">
         <Sidebar />
-        <main className="flex-1 ">{children}</main>
+        <main className="flex-1 overflow-y-scroll ">{children}</main>
       </div>
       <footer className="h-20 ">this is a player components</footer>
     </div>
