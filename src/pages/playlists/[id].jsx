c28172a5be7be9ba36a01f24c17ea25b9aa55be3
Samatar5 <samatar5mohamed@gmail.com>
2023-03-29 16:42:00 +0200

    checkpoint

--- src/pages/playlists/[id].jsx
+++ src/pages/playlists/[id].jsx
@@ -19,18 +19,49 @@ export default function Playlist() {
   if (isError) return <Layout>error...</Layout>;
   return (
     <Layout>
-      <div className="p-10">
-        <div className="flex gap-3">
-          <img
-            src={playlist.images(0).url}
-            alt="playlist image"
-            className="h-20 w-20 bg-red-500"
-          />
-          <div>
-            <p className="font-semibold text-text-dimmed">playlist</p>
-            <h2 className="text-3xl font-semibold">{playlist.name}</h2>
-          </div>
+      <div className="flex items-end gap-3 bg-gradient-to-b from-primary/60 to-bg-dimmed p-10">
+        <img
+          src={playlist.images[0]?.url}
+          alt="playlist image"
+          className="h-60 w-60 flex-shrink-0"
+        />
+        <div>
+          <p className="font-semibold text-text-dimmed">playlist</p>
+          <h2 className="text-6xl font-black">{playlist.name}</h2>
         </div>
+      </div>
+      <div className="p-10">
+        <table className="w-full bg-gray-900">
+          <tbody>
+            {playlist.tracks.items.map((item, index) => (
+              <tr
+                key={item.id}
+                className=" h-16 whitespace-nowrap text-sm text-text-dimmed hover:bg-text-dimmed/10"
+              >
+                <td className="pl-4 text-base">{index + 1}</td>
+                <td>
+                  <div className="flex gap-4  ">
+                    <img
+                      src={item.track.album.images[0].url}
+                      alt=""
+                      className="h-12 w-12"
+                    />
+
+                    <div>
+                      <h4 className="text-ellipsis text-text">
+                        {item.track.name}
+                      </h4>
+                      <p>{item.track.artists[0].name}</p>
+                    </div>
+                  </div>
+                </td>
+
+                <td>{item.track.album.name}</td>
+                <td className="pr-4">{item.track.duration_ms}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </Layout>
   );
